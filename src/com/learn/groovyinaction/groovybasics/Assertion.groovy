4f297d14c741c732a5b1cf28cf9e66753e2cabff
Jerry Li <jerry@Jerrys-MacBook-Pro.local>
2017-10-04 16:16:01 +0800

    Initial commit

---
+++ src/com/learn/groovyinaction/groovybasics/Assertion.groovy
@@ -0,0 +1,8 @@
+package com.learn.groovyinaction.groovybasics
+assert (true)
+//or
+assert true
+
+assert (1 == 1)
+//or
+assert 1 ==1
---
+++ src/com/learn/groovyinaction/groovybasics/Book.groovy
@@ -0,0 +1,13 @@
+package com.learn.groovyinaction.groovybasics
+
+class Book {
+    private String title;
+
+    Book(String title) {
+        this.title = title
+    }
+
+    String getTitle() {
+        return title
+    }
+}
